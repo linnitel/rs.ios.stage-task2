0dc0d44511c6133000a686441ef5f716c4978be1
Tothmure Isabel <tisabel@ox-p1.msk.21-school.ru>
2021-05-14 20:02:31 +0300

    fixes: done first exersise

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,12 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    UInt8 reverse = 0;
+    for (int pos = 7; pos >= 0 && n; pos--) {
+        if (n & 1) {
+            reverse = reverse | (1 << pos);
+        }
+        n >>= 1;
+    }
+    return reverse;
 }
