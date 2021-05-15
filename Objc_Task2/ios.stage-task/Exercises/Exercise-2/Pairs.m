c6c574b29e7079fd4e8da974bcad2740f3a019de
Tothmure Isabel <tisabel@ox-p1.msk.21-school.ru>
2021-05-15 21:14:23 +0300

    completed second task

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -2,8 +2,19 @@
 
 @implementation Pairs
 
+
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    NSInteger pairsNum = 0;
+    NSArray *sortedArray = [array sortedArrayUsingSelector:@selector(compare:)];
+    NSInteger count = [sortedArray count];
+    for (NSInteger index = 0; index < count - 1; index++) {
+        for (NSInteger indexTwo = index + 1; indexTwo < count; indexTwo++) {
+            if (abs([sortedArray[index] intValue] - [sortedArray[indexTwo] intValue]) == [number intValue]) {
+                pairsNum++;
+            }
+        }
+    }
+    return pairsNum;
 }
 
 @end
