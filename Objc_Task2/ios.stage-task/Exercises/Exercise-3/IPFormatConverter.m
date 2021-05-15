6676ea873a3df6b67d794ffbb4ceff9d4e8afc0d
Tothmure Isabel <tisabel@ox-p1.msk.21-school.ru>
2021-05-15 22:12:50 +0300

    completed task three

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,33 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    NSInteger count = [numbersArray count];
+    if (!numbersArray || count == 0) {
+        return @"";
+    }
+    NSInteger numOct = 4;
+    NSMutableString *temp = [NSMutableString new];
+    for (NSInteger index = 0; index < numOct; index++) {
+        if (index < count && [numbersArray[index] intValue] < 0) {
+            [temp setString:@"Negative numbers are not valid for input."];
+            break;
+        }
+        if (index < count && [numbersArray[index] intValue] > 225) {
+            [temp setString:@"The numbers in the input array can be in the range from 0 to 255."];
+            break;
+        }
+        if (index >= count) {
+            [temp appendFormat:@"%@", @"0"];
+        }
+        else {
+            [temp appendFormat:@"%@", numbersArray[index]];
+        }
+        if (index != numOct - 1) {
+            [temp appendFormat:@"%@", @"."];
+        }
+    }
+    NSString *IPv4 = [[NSString alloc] initWithString:temp];
+    return IPv4;
 }
 
 @end
